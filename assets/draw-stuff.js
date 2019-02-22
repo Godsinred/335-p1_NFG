fe1756d93dc2ee6cf03d55d996b168dddea05081
mmikulka <mattmikulka@gmail.com>
2019-02-21 21:52:15 -0800

    moved grid over

--- assets/draw-stuff.js
+++ assets/draw-stuff.js
@@ -11,7 +11,7 @@ function draw_rect( ctx, stroke, fill )
     ctx.strokeStyle = stroke;
     ctx.fillStyle = fill;
     ctx.lineWidth = 5;
-    ctx.rect(75, 50, canvas.width - 150, canvas.height - 100);
+    ctx.rect(20, 15, canvas.width, canvas.height);
     ctx.stroke();
     ctx.fill();
     ctx.restore( );
@@ -28,20 +28,20 @@ function draw_grid( rctx, rminor, rmajor, rstroke, rfill  )
     for ( var ix = 0; ix < width; ix += rminor )
     {
         rctx.beginPath( );
-        rctx.moveTo( ix, 0 );
-        rctx.lineTo( ix, height );
+        rctx.moveTo( ix + 20, 15 );
+        rctx.lineTo( ix+ 20, height );
         rctx.lineWidth = ( ix % rmajor == 0 ) ? 0.5 : 0.25;
         rctx.stroke( );
-        if ( ix % rmajor == 0 ) { rctx.fillText( ix, ix, 10 ); }
+        if ( ix % rmajor == 0 ) { rctx.fillText( ix/5, ix + 15, 10 ); }
     }
     for ( var iy = 0; iy < height; iy += rminor )
     {
         rctx.beginPath( );
-        rctx.moveTo( 0, iy );
-        rctx.lineTo( width, iy );
+        rctx.moveTo( 20, iy + 15 );
+        rctx.lineTo( width, iy + 15 );
         rctx.lineWidth = ( iy % rmajor == 0 ) ? 0.5 : 0.25;
         rctx.stroke( );
-        if ( iy % rmajor == 0 ) {rctx.fillText( iy, 0, iy + 10 );}
+        if ( iy % rmajor == 0 ) {rctx.fillText( iy /5, 0, iy + 20 );}
     }
     rctx.restore( );
 }
