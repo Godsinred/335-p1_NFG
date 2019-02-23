fb6f5c1f1a9c95d8f95e0544a9f3466e84a6c8ea
Jonathan_Ishii <jtishii@yahoo.com>
2019-02-22 21:34:17 -0800

    Totally fixed the size and grid of the graph.

--- assets/draw-stuff.js
+++ assets/draw-stuff.js
@@ -23,8 +23,8 @@ function draw_grid( rctx, rminor, rmajor, rstroke, rfill  )
     rctx.save( );
     rctx.strokeStyle = rstroke;
     rctx.fillStyle = rfill;
-    let width = rctx.canvas.width -20;
-    let height = rctx.canvas.height - 15;
+    let width = rctx.canvas.width - 25;
+    let height = rctx.canvas.height - 20;
     for ( var ix = 0; ix < width - 10; ix += rminor )
     {
         rctx.beginPath( );
@@ -32,16 +32,16 @@ function draw_grid( rctx, rminor, rmajor, rstroke, rfill  )
         rctx.lineTo( ix + 20, height);
         rctx.lineWidth = ( ix % rmajor == 0 ) ? 0.5 : 0.25;
         rctx.stroke( );
-        if ( ix % rmajor == 0 ) { rctx.fillText( ix/5, ix + 15, 10 ); }
+        if ( ix % rmajor == 0 ) { rctx.fillText( ix / 5, ix + 15, 10 ); }
     }
-    for ( var iy = 0; iy < height -10; iy += rminor )
+    for ( var iy = 0; iy < height - 10; iy += rminor )
     {
         rctx.beginPath( );
         rctx.moveTo( 20, iy + 15 );
         rctx.lineTo( width + 5, iy + 15 );
         rctx.lineWidth = ( iy % rmajor == 0 ) ? 0.5 : 0.25;
         rctx.stroke( );
-        if ( iy % rmajor == 0  && iy != 0) {rctx.fillText( iy /5, 0, iy + 20 );}
+        if ( iy % rmajor == 0  && iy != 0) {rctx.fillText( iy / 5, 0, iy + 20 );}
     }
     rctx.restore( );
 }
@@ -79,23 +79,23 @@ function cella_90( ctx, num_canvas_cells)
             var cell_value = 0;
 
             // checks to make sure that you don't index out of the array in the bottom row
-            if(i !== num_canvas_cells - 1)
+            if (i !== num_canvas_cells - 1)
             {
                 // checks the value of above and left
-                if(array[i - 1][j - 1] === 1)
+                if (array[i - 1][j - 1] === 1)
                 {
                     cell_value += 4;
                 }
 
                 // checks the value of above and right
-                if(array[i - 1][j + 1] === 1)
+                if (array[i - 1][j + 1] === 1)
                 {
                     cell_value += 1;
                 }
             }
 
             // checks the value of right above
-            if(array[i - 1][j] === 1)
+            if (array[i - 1][j] === 1)
             {
                 cell_value += 2;
             }
@@ -104,7 +104,7 @@ function cella_90( ctx, num_canvas_cells)
             //console.log(color);
 
             // colors the square if it is 1 i.e. black, since the color is already white
-            if(color === 1)
+            if (color === 1)
             {
                 array[i][j] = 1;
                 
